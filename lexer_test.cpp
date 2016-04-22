dc58e283620a11423edee62767b138b2edfcd402
Xiaohui Wang <wang502@indiana.edu>
2016-04-22 14:32:29 -0400

    dummy lexer added

--- lexer_test.cpp
+++ lexer_test.cpp
@@ -3,6 +3,8 @@
 #include <cctype>
 
 #define PEEK (sql_state->buffer[sql_state->offset])
+#define SKIP (sql_state->offset++)
+
 using namespace std;
 
 struct sqlstate{
@@ -14,8 +16,15 @@ bool is_identifier(char c){
   return isalpha(c) || isdigit(c) || c == '_';
 }
 
-bool is_alphe(char c){
+bool is_alpha(char c){
   return isalpha(c);
+}
+
+bool is_space(char c){
+  return c == ' ' || c == '\t' || c == '\v' || c == '\f';
+}
+bool is_all(char c){
+  return c == '*';
 }
 
 void lexer_alpha(sqlstate *sql_state){
@@ -29,14 +38,25 @@ void lexer_alpha(sqlstate *sql_state){
 
 void lexer_next(sqlstate *sql_state){
   char c = PEEK;
-  if (isalpha(c)) lexer_alpha(sql_state);
+  //cout<<"peek: "<<c<<endl;
+  if (is_alpha(c)) {
+    lexer_alpha(sql_state);
+  }
+  c = PEEK;
+  //cout<<"peek: "<<c<<endl;
+  if (is_space(c)) SKIP; cout<<"skip once\n";
+  c = PEEK;
+  //cout<<"peek: "<<c<<endl;
+  if (is_all(c)) SKIP; cout<<"skip once\n";
 }
 
 void lexer(char *sql){
   struct sqlstate sql_state;
   sql_state.buffer = sql;
   sql_state.offset = 0;
-  lexer_next(&sql_state);
+  while (sql_state.offset <= strlen(sql_state.buffer)-1){
+    lexer_next(&sql_state);
+  }
 }
 
 int main(){
